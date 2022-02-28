ef0574418e451926a1e11cf9c491b4d764b7d527
Prathamesh Gaikwad <prathamesh@leewayhertz.com>
2022-02-28 17:09:20 +0530

    Change in port

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ app.post("/sign-up", (req, res) => {
   });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port 3005`)
 })
